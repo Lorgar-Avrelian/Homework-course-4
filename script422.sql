229956cf8e4e85076d1cd20f72cc24ded7724119
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-09-23 11:59:01 +0300

    Homework done

--- script422.sql
+++ script422.sql
@@ -1,16 +1,16 @@
+CREATE TABLE car (
+    id SERIAL PRIMARY KEY IDENTITY,
+    brand TEXT UNIQUE,
+    model TEXT NOT NULL,
+    price MONEY CHECK (price > 0)
+);
+
 CREATE TABLE human (
     id SERIAL PRIMARY KEY IDENTITY,
     name TEXT UNIQUE,
     age INTEGER CHECK (age >= 0) AND (age < 120),
     drivingLicense BOOLEAN,
     carId INTEGER NOT NULL REFERENCES car (Id)
-);
-
-CREATE TABLE car (
-    id SERIAL PRIMARY KEY IDENTITY,
-    brand TEXT UNIQUE,
-    model TEXT NOT NULL,
-    price MONEY CHECK (price > 0)
 );
 
 SELECT human.id, human.name, human.age, human.drivingLicense, car.brand, car.model, car.price
