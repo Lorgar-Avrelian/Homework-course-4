e36822890cb62d4ab5d0d1db662f1699031cee86
Lorgar-Avrelian <victor-14-244@mail.ru>
2023-09-30 11:13:04 +0300

    Homework done

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,19 +1,18 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.hogwarts.school.service.PortInfo;
 
 @RestController
 @RequestMapping("/getPort")
 public class InfoController {
-    @Autowired
-    private PortInfo portInfo;
+    @Value("${server.port}")
+    private int port;
 
     @GetMapping
     public int getPortNumber() {
-        return portInfo.portInfo();
+        return port;
     }
 }
--- src/main/java/ru/hogwarts/school/service/PortInfo.java
+++
@@ -1,5 +0,0 @@
-package ru.hogwarts.school.service;
-
-public interface PortInfo {
-    int portInfo();
-}
